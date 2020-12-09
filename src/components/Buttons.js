be98ae9f8efa43437e354e48335618f58b073f70
zdsmi <zdsmith001@gmail.com>
2020-12-09 14:52:15 -0600

    created skeletons for all app components

---
+++ src/components/Buttons.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Button from './buttons';
+
+class Buttons extends Component {
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,2 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+class Display extends Component {
+    
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+} 

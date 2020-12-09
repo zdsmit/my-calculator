40f146984953e4ecd64c470150715f21afbb0fb2
zdsmi <zdsmith001@gmail.com>
2020-12-09 14:44:36 -0600

    Started building out component files

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Display from './Display';
+import Buttons from './Buttons';
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div>
+                <Display />
+            </div>
+        )
+    }
+}

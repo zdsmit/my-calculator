cc18beb77b25f7cb7eb2eccef3b06717b943a6c2
zdsmi <zdsmith001@gmail.com>
2020-12-27 10:27:12 -0600

    got calculator buttons to render side by side

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,22 +6,28 @@ export default class Buttons extends Component {
     render() {
         return (
             <div>
-                <Button value="1"/>
-                <Button value="2"/>
-                <Button value="3"/>
-                <Button value="+"/>
-                <br />
+                <div class="btn-group">
+                  <Button value="1"/>
+                  <Button value="2"/>
+                  <Button value="3"/>
+                  <Button value="+"/>
+                  <br />
+                </div>
 
-                <Button value="4"/>
-                <Button value="5"/>
-                <Button value="6"/>
-                <Button value="-"/>
-                <br />
+                <div class="btn-group">
+                  <Button value="4"/>
+                  <Button value="5"/>
+                  <Button value="6"/>
+                  <Button value="-"/>
+                  <br />
+                </div>
 
-                <Button value="7"/>
-                <Button value="8"/>
-                <Button value="9"/>
-                <Button value="*"/>
+                <div class="btn-group">
+                  <Button value="7"/>
+                  <Button value="8"/>
+                  <Button value="9"/>
+                  <Button value="*"/>
+                </div>
             </div>
         )
     }

b06339f95d573e0d0fc96e7df2c2fbc77259725b
zdsmi <zdsmith001@gmail.com>
2021-01-04 11:43:46 -0600

    keypad now renders in correct format

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,35 +5,38 @@ export default class Buttons extends Component {
 
     render() {
         return (
-            <div>
+            <div class="calc-buttons">
+
                 <div class="btn-group">
-                  <Button value="1"/>
-                  <Button value="2"/>
-                  <Button value="3"/>
-                  <Button value="+"/>
+                  <Button value="1" />
+                  <Button value="4" />
+                  <Button value="7" />
+                  <Button value="+" />
                   <br />
                 </div>
 
                 <div class="btn-group">
-                  <Button value="4"/>
+                  <Button value="2"/>
                   <Button value="5"/>
-                  <Button value="6"/>
+                  <Button value="8"/>
                   <Button value="-"/>
                   <br />
                 </div>
 
                 <div class="btn-group">
-                  <Button value="7"/>
-                  <Button value="8"/>
+                  <Button value="3"/>
+                  <Button value="6"/>
                   <Button value="9"/>
                   <Button value="*"/>
+                  <br />
                 </div>
 
-                <div>
-                  <Button value="/"/>
+                <div class="btn-group">
                   <Button value="0"/>
-                  <Button value="."/>
                   <Button value="="/>
+                  <Button value="."/>
+                  <Button value="/"/>
+                  <Button value="C"/>
                 </div>
             </div>
         )
